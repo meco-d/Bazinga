2d2dd29cfcfac4ecc78a5fc14f499b72047e6bb7
MonsieurPoopyButthole <rubinlemeti@gmail.com>
2024-05-31 23:03:53 +0200

    update router

--- backend/src/v1/router.py
+++ backend/src/v1/router.py
@@ -2,14 +2,9 @@ from fastapi import APIRouter
 
 # from src.endpoints import articles
 from src.v1.modules.auth import auth_endpoints
-from src.v1.modules.firebase import firebase_messaging_endpoints
 from src.v1.modules.users import users
 
 api_router = APIRouter()
 
-
-api_router.include_router(
-    firebase_messaging_endpoints.router, prefix="/chat", tags=["chat"]
-)
 api_router.include_router(auth_endpoints.router, prefix="", tags=["auth"])
 api_router.include_router(users.router, prefix="", tags=["users"])
