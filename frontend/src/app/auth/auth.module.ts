6ad7a860f8af98981e828594a708b288c831303d
Meco <mecodine15@gmail.com>
2024-06-01 11:46:45 +0200

    adding login page

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from '@angular/common';
 import * as fromComponents from './components';
 import {AuthRoutingModule} from "./auth-routing.module";
 import {HttpClientModule} from "@angular/common/http";
+import {CardModule} from "primeng/card";
+import {Button} from "primeng/button";
 
 
 @NgModule({
   declarations: [...fromComponents.components],
-  imports: [CommonModule, AuthRoutingModule, HttpClientModule,]
+  imports: [CommonModule, AuthRoutingModule, HttpClientModule, CardModule, Button,]
 })
 export class AuthModule {
 }
