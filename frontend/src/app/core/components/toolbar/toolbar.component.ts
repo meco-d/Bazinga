9b85697055a6f28897e581a43f44223e514dcc2c
jlika <similika25@gmail.com>
2024-06-01 22:45:14 +0200

    initial commit for navigation

--- frontend/src/app/core/components/toolbar/toolbar.component.ts
+++ frontend/src/app/core/components/toolbar/toolbar.component.ts
@@ -1,26 +1,45 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {MENU, MenuItem} from "@core/components/navigation/menu";
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
 
   userEmail: string = '';
   userFullName: string = '';
-  @Output() sidenavToggleClick: EventEmitter<any> = new EventEmitter<any>();
+  today = new Date();
+  nextWeek: Date = new Date();
+  currentComponent: any;
+  hasTitle: boolean = false;
+  title: string = '';
+  maximize = true;
+  readonly menu = MENU;
+  filteredMenu: MenuItem[] = [];
 
   constructor(private router: Router) {
+    this.nextWeek.setDate(this.today.getDate() + 7);
+    this.filteredMenu = this.menu.filter(menuItem => this.menuItemCanLoad(menuItem));
+  }
+
+  ngOnInit() {
+    this.hasTitle = false;
+    this.title = 'ZAP MAP';
   }
 
   logout() {
     this.router.navigate(['login/']).finally();
   }
 
-  toggleSidenav(): void {
-    this.sidenavToggleClick.emit();
+  private menuItemCanLoad(menuItem: MenuItem) {
+    if (menuItem && menuItem.children) {
+      menuItem.children = menuItem.children.filter(ch => ch);
+      return true;
+    }
+    return false;
   }
 
 }
